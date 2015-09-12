ff57b80f2c0035c9360960c9b7f4520f0511bb30
Azat Almeev <azat.almeev@gmail.com>
2015-09-12 22:05:40 +0300

    Initial Commit

---
+++ NearestPizza/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  NearestPizza
+//
+//  Created by Azat Almeev on 12.09.15.
+//  Copyright (c) 2015 Azat Almeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ NearestPizza/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NearestPizza
+//
+//  Created by Azat Almeev on 12.09.15.
+//  Copyright (c) 2015 Azat Almeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
